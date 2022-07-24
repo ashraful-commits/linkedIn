499e609b6474a64b63621e850774522ad4e2c284
Md.Ashraful Alam <101570492+ashraful-commits@users.noreply.github.com>
2022-07-24 19:19:10 +0600

    changes

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,56 +1,43 @@
+const setAlert = (msg, type) => {
+  `<h5 class="alert alert-${type} text-center">${msg}</h5>`;
+};
 
-const setAlert =(msg,type)=>{
-    `<h5 class="alert alert-${type} text-center">${msg}</h5>`
-}
+const setDataLS = (key, value) => {
+  let data = [];
+  if (localStorage.getItem(key)) {
+    data = JSON.parse(localStorage.getItem(key));
+  }
+  data.push(value);
+  return localStorage.setItem(key, JSON.stringify(data));
+};
 
+const getItemLs = (key) => {
+  if (localStorage.getItem(key)) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+};
 
-
-const setDataLS =(key,value)=>{
-let data =[];
-if(localStorage.getItem(key)){
-    data =JSON.parse(localStorage.getItem(key));
-}
-data.push(value);
-return localStorage.setItem(key,JSON.stringify(data));
-}
-
-
-const getItemLs = (key)=>{
-    if(localStorage.getItem(key)){
-        return JSON.parse(localStorage.getItem(key));
-    }
-}
-
-
-const updateLsData = (key,array) => {
-    localStorage.setItem(key,JSON.stringify(array))
-}
-
-
-
+const updateLsData = (key, array) => {
+  localStorage.setItem(key, JSON.stringify(array));
+};
 
 const timeCounter = (postTime) => {
+  let currentTime = Date.now();
 
-    let currentTime = Date.now();
+  let timeDiff = currentTime - postTime;
 
-    let timeDiff = (currentTime - postTime);
+  let TotalSecond = Math.floor(timeDiff / 1000);
+  let Total_min = Math.floor(TotalSecond / 60);
+  let TotalHour = Math.floor(Total_min / 60);
+  let totalDay = Math.floor(TotalHour / 24);
 
-    let TotalSecond =Math.floor(timeDiff / 1000);
-    let Total_min = Math.floor(TotalSecond / 60);
-    let TotalHour =Math.floor( Total_min / 60);
-    let totalDay = Math.floor(TotalHour / 24);
-
-    if (TotalSecond <= 60) {
-        return `${ TotalSecond } sec ago`;
-    } else if (TotalSecond > 60) {
-        return `${Total_min} min ago`;
-    } else if (Total_min > 60) {
-        return `${ TotalHour} hour ago`;
-
-    } else if (TotalHour > 24) {
-        return `${ totalDay} day ago`;
-    }
-    
-
-    
-}
+  if (TotalSecond <= 60) {
+    return `${TotalSecond} sec ago`;
+  } else if (TotalSecond >= 60) {
+    return `${Total_min} min ago`;
+  } else if (Total_min >= 60) {
+    return `${TotalHour} hour ago`;
+  } else if (TotalHour >= 24) {
+    return `${totalDay} day ago`;
+  }
+};
